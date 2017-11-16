729196cb3c7dbdabc4d33cc0e94ea4c8550397c1
Joseph Sams <joe.mac@Josephs-iMac.local>
2017-11-16 18:10:54 -0500

    Updated bash scripts for docker containers

--- scripts/backend.sh
+++ scripts/backend.sh
@@ -1,2 +1,2 @@
-sudo docker run -d --name=api --restart=always --net=host --log-opt max-size=1g -e BACKEND_PORT=3001 warlogs/core sh -c "npm start"
+sudo docker run -d --name=api --restart=always --log-opt max-size=1g -e BACKEND_PORT=3001 -p 80:3001 warlogs/core sh -c "npm start"
 sudo docker start api
